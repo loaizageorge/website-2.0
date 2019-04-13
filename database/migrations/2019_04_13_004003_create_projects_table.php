72dd74b2f003618db0eefba3f56a77d008a2000e
george <gloaiza@gmail.com>
2019-04-12 20:49:03 -0400

    Create tables

---
+++ database/migrations/2019_04_13_004003_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('title');
+            $table->char('image');
+            $table->char('source');
+            $table->char('demo');
+            $table->char('demo_text');
+            $table->mediumText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
