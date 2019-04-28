dab9ddd8e41dda7280cc61f2025c34907072e228
george <loaizageorge@gmail.com>
2019-04-27 21:22:18 -0400

    Add semantic ui to dashboard. Add create project section

--- resources/js/components/Dashboard/Dashboard.jsx
+++ resources/js/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Container, Label, Input } from 'semantic-ui-react';
 
 function handleSubmit(event) {
   event.preventDefault();
@@ -15,21 +16,76 @@ function handleSubmit(event) {
 function Dashboard() {
   console.log('test');
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">
-      Name:
+    <>
+    <Container>
+    <form className="ui form" onSubmit={handleSubmit}>
+      <div className="field">
+      <label>Name</label>
         <input type="text" name="name" />
-      </label>
+      </div>
 
-      <label htmlFor="order">
-      Order:
+      <div className="field">
+      <label>Order</label>
         <input type="number" name="order" />
-      </label>
+      </div>
 
+      <div className="field">
       <input type="submit" value="Submit" />
+      </div>
     </form>
+    </Container>
+
+    <Container>
+      <AddProject/>
+    </Container>
+    </>
   );
 }
+
+function AddProject() {
+  return (
+    // name, order, demo link, demo text, source link, description, image, section
+      <form className="ui form" onSubmit={handleSubmit}>
+        <div className="field">
+        <label>Name</label>
+          <input type="text" name="name" />
+        </div>
+  
+        <div className="field">
+        <label>Order</label>
+          <input type="number" name="order" />
+        </div>
+
+        <div className="field">
+        <label>Demo Link</label>
+          <input type="text" name="demo_link" />
+        </div>
+
+        <div className="field">
+        <label>Demo Text</label>
+          <input type="text" name="demo_text" />
+        </div>
+
+        <div className="field">
+        <label>Source Link</label>
+          <input type="text" name="source_link" />
+        </div>
+
+        <div className="field">
+        <label>Description</label>
+          <textarea name="description" />
+        </div>
+
+        <div className="field">
+        <label>Image</label>
+          <input type="file" name="image" />
+        </div>
+
+        <input type="submit" value="Submit" />
+      </form>
+  );
+}
+
 if (document.getElementById('dashboard')) {
   ReactDOM.render(<Dashboard />, document.getElementById('dashboard'));
 }
